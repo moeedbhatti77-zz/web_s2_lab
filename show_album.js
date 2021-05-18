23f78c8184812a9e9e4348b65289097beac04886
Moeed <moeedbhatti419@gmail.com>
2021-05-18 12:59:10 +0500

    album del

--- show_album.js
+++ show_album.js
@@ -2,23 +2,42 @@ $(function () {
     var url = window.location.href;
     var id = url.split('=');
     show(id[1]);
-    $('.add_album').click(function(){
-        $.ajax({
-            method : 'POST',
-            url: "https://jsonplaceholder.typicode.com/albums?userId="+id[1],
-            data: "data",
-            dataType: "dataType",
-            success: function (response) {
-                
-            }
-        });
-    });
+    // $('.add_album').click(function(){
+    //     $.ajax({
+    //         method : 'POST',
+    //         url: "https://jsonplaceholder.typicode.com/albums?userId="+id[1],
+    //         data: "data",
+    //         dataType: "dataType",
+    //         success: function (response) {
+    //         }
+    //     });
+    // });
 });
 function show(id) { 
-    fetch('https://jsonplaceholder.typicode.com/albums?userId='+id)
-    .then(response => response.json())
-    .then(json => json.forEach(function(val) {
-            $('#t_body').append("<tr><td>"+val.id+"</td><td>"+val.title+"</td><td><button class='btn btn-primary'>Update</button></td><td><button class='btn btn-primary'>Delete</button></td></tr>");
-    })
-    );
+    $.ajax({
+        method : "GET",
+        url : 'https://jsonplaceholder.typicode.com/albums?userId='+id,
+        success : function (response){
+            response.forEach(function(val){
+                $('#t_body').append("<tr><td>"+val.id+"</td><td>"+val.title+"</td><td><button data-id="+val.id+" class='btn btn-primary'>Update</button></td><td><button data-id="+val.id+" class='btn btn-primary del' onclick='del("+val.id+")'>Delete</button></td></tr>");
+            });
+        }
+    });
+    $('.del').click(function() {
+        var id = $(this).attr('data-id'); 
+        console.log(id);
+        del(id);
+    });
+}
+function del (id){
+    console.log(id);
+    $.ajax({
+        method : 'DELETE',
+        url : "https://jsonplaceholder.typicode.com/albums/"+id,
+        success : function(){ 
+            alert('Deleted album '+id);
+            temp = "[data-id="+id+"]";
+            $(temp).parent().parent().hide();
+    }
+    });
 }
--- users.js
+++ users.js
@@ -9,7 +9,6 @@ function show_users(){
             response.forEach(function(temp) {
                 $('#t_body').append("<tr ><td>"+temp.name+"</td><td>"+temp.email+"</td><td><a href='./user_album.html?id="+temp.id+"'><button data-id="+temp.id+" class='btn btn-primary albums'>Albums</button></a></td><td><button class='btn btn-primary update' >Update</button></td></tr>");
             });
-
         }
     });
 }
