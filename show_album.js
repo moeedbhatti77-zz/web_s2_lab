eeff01e230c12f88c7da226958803eca30d26854
Moeed <moeedbhatti419@gmail.com>
2021-05-18 12:10:27 +0500

    dumb changes again

--- show_album.js
+++ show_album.js
@@ -2,12 +2,23 @@ $(function () {
     var url = window.location.href;
     var id = url.split('=');
     show(id[1]);
+    $('.add_album').click(function(){
+        $.ajax({
+            method : 'POST',
+            url: "https://jsonplaceholder.typicode.com/albums?userId="+id[1],
+            data: "data",
+            dataType: "dataType",
+            success: function (response) {
+                
+            }
+        });
+    });
 });
 function show(id) { 
     fetch('https://jsonplaceholder.typicode.com/albums?userId='+id)
     .then(response => response.json())
     .then(json => json.forEach(function(val) {
-            $('#t_body').append("<tr><td>"+val.id+"</td><td>"+val.title+"</td><td><button class='btn btn-primary'>Update</button></td></tr>");
+            $('#t_body').append("<tr><td>"+val.id+"</td><td>"+val.title+"</td><td><button class='btn btn-primary'>Update</button></td><td><button class='btn btn-primary'>Delete</button></td></tr>");
     })
     );
 }
--- users.js
+++ users.js
@@ -7,17 +7,9 @@ function show_users(){
         url: "https://jsonplaceholder.typicode.com/users",
         success: function (response) {
             response.forEach(function(temp) {
-                $('#t_body').append("<tr ><td>"+temp.name+"</td><td>"+temp.email+"</td><td><a href='./temp.html?id="+temp.id+"'><button data-id="+temp.id+" class='btn btn-primary albums'>Albums</button></a></td><td><button class='btn btn-primary update' >Update</button></td></tr>");
+                $('#t_body').append("<tr ><td>"+temp.name+"</td><td>"+temp.email+"</td><td><a href='./user_album.html?id="+temp.id+"'><button data-id="+temp.id+" class='btn btn-primary albums'>Albums</button></a></td><td><button class='btn btn-primary update' >Update</button></td></tr>");
             });
-            // $('.albums').click(function(b){
-            //     var id = $(this).attr('data-id');
-            //     show_albums(id);
-            // });
+
         }
     });
 }
-// function show_albums(id){
-//     fetch('https://jsonplaceholder.typicode.com/albums?userId='+id)
-//   .then(response => response.json())
-//   .then(json => console.log(json))
-// }
