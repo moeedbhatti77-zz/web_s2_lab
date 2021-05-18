6993e2fc61f4ce1862923074bac73d48f03318cf
Moeed <moeedbhatti419@gmail.com>
2021-05-18 09:59:37 +0500

    show users

--- users.js
+++ users.js
@@ -1,3 +1,15 @@
-$(function() {
-    
+$(document).ready(function () {
+    show_users();
 });
+function show_users(){
+    $.ajax({
+        meyhod : 'GET',
+        url: "https://jsonplaceholder.typicode.com/users",
+        success: function (response) {
+            response.forEach(function(temp) {
+                // console.log(temp.name);
+                $('#t_body').append("<tr><td>"+temp.name+"</td><td>"+temp.email+"</td></tr>");
+            });
+        }
+    });
+}
