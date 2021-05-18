997abe80c367868fa7813cf272fb70e9afbb3df6
Moeed <moeedbhatti419@gmail.com>
2021-05-18 17:10:15 +0500

    99.99% done

--- show_album.js
+++ show_album.js
@@ -2,16 +2,19 @@ $(function () {
     var url = window.location.href;
     var id = url.split('=');
     show(id[1]);
-    // $('.add_album').click(function(){
-    //     $.ajax({
-    //         method : 'POST',
-    //         url: "https://jsonplaceholder.typicode.com/albums?userId="+id[1],
-    //         data: "data",
-    //         dataType: "dataType",
-    //         success: function (response) {
-    //         }
-    //     });
-    // });
+    $('.add_ablum').click(function(){
+
+        title = $('#title').val();
+        val = {userId : "1",title : "hello world"};
+        $.ajax({
+            method : 'POST',
+            url: "https://jsonplaceholder.typicode.com/albums?userId="+id[1],
+            data: val,
+            // success: function (response) {
+            //     console.log(response);
+            // }
+        });
+    });
 });
 function show(id) { 
     $.ajax({
@@ -19,18 +22,25 @@ function show(id) {
         url : 'https://jsonplaceholder.typicode.com/albums?userId='+id,
         success : function (response){
             response.forEach(function(val){
-                $('#t_body').append("<tr><td>"+val.id+"</td><td>"+val.title+"</td><td><button data-id="+val.id+" class='btn btn-primary'>Update</button></td><td><button data-id="+val.id+" class='btn btn-primary del' onclick='del("+val.id+")'>Delete</button></td></tr>");
+                $('#t_body').append("<tr><td>"+val.id+"</td><td>"+val.title+"</td><td><button data-id="+val.id+" class='btn btn-primary update')'>Update</button></td><td><button data-id="+val.id+" class='btn btn-primary del' onclick='del("+val.id+")'>Delete</button></td></tr>");
+            });
+            $('.update').click(function(){
+                temp = $(this).parent().parent().children(); 
+                td = temp[1];
+                {
+                    var title = prompt("Enter New Title",td.innerText);
+                    if(title == null || title == '')
+                    {
+                        alert('Cannot enter empty value');
+                    }else if (title != null) {
+                        td.innerText = title;
+                    }
+                }
             });
         }
     });
-    $('.del').click(function() {
-        var id = $(this).attr('data-id'); 
-        console.log(id);
-        del(id);
-    });
 }
 function del (id){
-    console.log(id);
     $.ajax({
         method : 'DELETE',
         url : "https://jsonplaceholder.typicode.com/albums/"+id,
--- users.js
+++ users.js
@@ -9,6 +9,18 @@ function show_users(){
             response.forEach(function(temp) {
                 $('#t_body').append("<tr ><td>"+temp.name+"</td><td>"+temp.email+"</td><td><a href='./user_album.html?id="+temp.id+"'><button data-id="+temp.id+" class='btn btn-primary albums'>Albums</button></a></td><td><button class='btn btn-primary update' >Update</button></td></tr>");
             });
-        }
-    });
+            $('.update').click(function(){
+                tr = $(this).parent().parent().children();
+                {
+                    var person = prompt("Enter New Email",tr[1].innerText);
+                    if(person == null || person == '')
+                    {
+                        alert('Cannot enter empty value');
+                    }else if (person != null) {
+                        tr[1].innerText = person;
+                    }
+                }
+            });
+        } 
+    });   
 }
